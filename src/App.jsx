8e84f80a1ac28d751ae8ac10d3f7e36eb32dffa0
unknown <nur.mnzaman@gmail.com>
2023-09-19 00:42:50 +0600

    [style] app structure

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,13 @@ import MiddleSection from "../src/layout/MiddleSection";
 function App() {
   return (
     <>
-      <Header />
-      <Fragment />
-      <MiddleSection />
+      <section className="max-container flex justify-center items-center bg-green-100 padding">
+        <section className="max-w-screen-sm padding flex justify-center items-center flex-col border border-gray-300 rounded-lg shadow-2xl">
+          <Header className="padding"/>
+          <Fragment className="padding"/>
+          <MiddleSection className="padding"/>
+        </section>
+      </section>
     </>
   );
 }
